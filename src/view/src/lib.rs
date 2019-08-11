c84d7595d97d1bf3f43239634da0a78f88e03e33
lazydancer <jrpucula@gmail.com>
2019-08-10 20:46:35 -0400

    Connect python blueprint to rust

--- src/view/src/lib.rs
+++ src/view/src/lib.rs
@@ -53,6 +53,8 @@ fn radians(dir: Direction) -> f32 {
 }
 
 struct Assets {
+    spritebatch: graphics::spritebatch::SpriteBatch,
+
     arrow_up_active: graphics::Image,
     arrow_right_active: graphics::Image,
     arrow_down_active: graphics::Image,
@@ -104,7 +106,11 @@ impl Assets {
             graphics::Image::new(ctx, "/split_horizontal_inactive.png")?;
         let split_vertical_inactive = graphics::Image::new(ctx, "/split_vertical_inactive.png")?;
 
+        let image = graphics::Image::new(ctx, "/spritesheet.png")?;
+        let spritebatch = graphics::spritebatch::SpriteBatch::new(image);
+
         Ok(Assets {
+            spritebatch,
             arrow_up_active,
             arrow_right_active,
             arrow_down_active,
@@ -187,6 +193,68 @@ impl Assets {
             BlockType::Empty => &mut self.split_vertical_active,
         }
     }
+
+    fn spritesheet_loc(&self, block: &Block) -> graphics::Rect {
+        let (x, y) = match block.block_type {
+            BlockType::Arrow(dir) => {
+                if block.active {
+                    match dir {
+                        Direction::Up => (0.0, 0.0),
+                        Direction::Right => (0.25, 0.0),
+                        Direction::Down => (0.50, 0.0),
+                        Direction::Left => (0.75, 0.0),
+                    }
+                } else {
+                    match dir {
+                        Direction::Up => (0.0, 0.20),
+                        Direction::Right => (0.25, 0.20),
+                        Direction::Down => (0.50, 0.20),
+                        Direction::Left => (0.75, 0.20),
+                    }
+                }
+            }
+
+            BlockType::NotArrow(dir) => {
+                if block.active {
+                    match dir {
+                        Direction::Up => (0.0, 0.40),
+                        Direction::Right => (0.25, 0.40),
+                        Direction::Down => (0.50, 0.40),
+                        Direction::Left => (0.75, 0.40),
+                    }
+                } else {
+                    match dir {
+                        Direction::Up => (0.0, 0.60),
+                        Direction::Right => (0.25, 0.60),
+                        Direction::Down => (0.50, 0.60),
+                        Direction::Left => (0.75, 0.60),
+                    }
+                }
+            }
+
+            BlockType::Split(dir) => {
+                if block.active {
+                    match dir {
+                        Direction::Up => (0.25, 0.80),
+                        Direction::Right => (0.0, 0.80),
+                        Direction::Down => (0.25, 0.80),
+                        Direction::Left => (0.0, 0.80),
+                    }
+                } else {
+                    match dir {
+                        Direction::Up => (0.75, 0.0),
+                        Direction::Right => (0.5, 0.0),
+                        Direction::Down => (0.75, 0.0),
+                        Direction::Left => (0.5, 0.0),
+                    }
+                }
+            }
+
+            BlockType::Empty => (0.0, 0.0),
+        };
+
+        graphics::Rect::new(x, y, 0.25, 0.20)
+    }
 }
 
 fn draw_arrow(
@@ -203,7 +271,7 @@ fn draw_arrow(
     graphics::draw(ctx, image, drawparams)
 }
 
-fn pos_to_screen_coords(
+fn pos_to_screen(
     window_size: &(i32, i32),
     view_top_left: &(i32, i32),
     pos: (i32, i32),
@@ -267,17 +335,22 @@ impl event::EventHandler for MainState {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let now = Instant::now();
-        graphics::clear(ctx, [0.0, 0.0, 0.0, 1.0].into());
+        graphics::clear(ctx, graphics::BLACK);
 
         let assets = &mut self.assets;
 
         for (pos, block) in self.board.get_arrows() {
-            let coord =
-                pos_to_screen_coords(&self.display_size, &self.view_top_left, (pos.x, pos.y));
+            let coord = pos_to_screen(&self.display_size, &self.view_top_left, (pos.x, pos.y));
             if let Some(coord) = coord {
-                draw_arrow(assets, ctx, coord, block);
+                let image_rect = assets.spritesheet_loc(&block);
+                let drawparams = graphics::DrawParam::new().src(image_rect).dest(coord);
+                assets.spritebatch.add(drawparams);
+                //draw_arrow(assets, ctx, coord, block);
             }
         }
+
+        let parm = graphics::DrawParam::new().dest(Point2::new(0.0, 0.0));
+        graphics::draw(ctx, &assets.spritebatch, parm)?;
 
         // Finished drawing, show it all on the screen!
         graphics::present(ctx)?;
